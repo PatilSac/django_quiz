6d5c1cef374386c61f06583a604945d416e38572
PatilSac <sachin787521@gmail.com>
2020-01-09 22:16:21 +0400

    staging the contents

--- qv1/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Qv1Config(AppConfig):
-    name = 'qv1'
--- qv1/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Quizez
-
-urlpatterns = [
-
-    path ('quizes/', Quizez.as_view(), name='quizez')
-]
